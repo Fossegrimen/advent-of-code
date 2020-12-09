165a4378b0cc2e479b48abbf65bedc053c1dfbd6
Fossegrimen <68206123+Fossegrimen@users.noreply.github.com>
2020-12-09 17:51:35 +0100

    Added day 10

---
+++ day10a.cpp
@@ -0,0 +1,40 @@
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<size_t> joltageRatings;
+    joltageRatings.push_back(0);
+
+    size_t joltageRating = 0;
+
+    while (std::cin >> joltageRating)
+    {
+        joltageRatings.push_back(joltageRating);
+    }
+
+    std::sort(joltageRatings.begin(), joltageRatings.end());
+    joltageRatings.push_back(joltageRatings[joltageRatings.size() - 1] + 3);
+
+    size_t difference_1 = 0;
+    size_t difference_3 = 0;
+
+    for (size_t i = 1; i < joltageRatings.size(); i++)
+    {
+        if (joltageRatings[i] - joltageRatings[i - 1] == 1)
+        {
+            difference_1++;
+        }
+
+        if (joltageRatings[i] - joltageRatings[i - 1] == 3)
+        {
+            difference_3++;
+        }
+    }
+
+    std::cout << difference_1 * difference_3 << std::endl;
+
+    return 0;
+}
---
+++ day10b.cpp
@@ -0,0 +1,65 @@
+
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <unordered_map>
+#include <vector>
+
+uint64_t findPossibilities(std::unordered_map<size_t, uint64_t>& adapterArrangementsMap, std::vector<size_t>& joltageRatings, size_t n);
+bool validJoltageDiff(size_t a, size_t b);
+
+int main()
+{
+    std::vector<size_t> joltageRatings;
+    joltageRatings.push_back(0);
+
+    size_t joltageRating = 0;
+
+    while (std::cin >> joltageRating)
+    {
+        joltageRatings.push_back(joltageRating);
+    }
+
+    std::sort(joltageRatings.begin(), joltageRatings.end());
+    joltageRatings.push_back(joltageRatings[joltageRatings.size() - 1] + 3);
+
+    std::unordered_map<size_t, uint64_t> adapterArrangementsMap;
+    adapterArrangementsMap[joltageRatings.size() - 1] = 1;
+
+    std::cout << findPossibilities(adapterArrangementsMap, joltageRatings, 0) << std::endl;
+
+    return 0;
+}
+
+uint64_t findPossibilities(std::unordered_map<size_t, uint64_t>& adapterArrangementsMap, std::vector<size_t>& joltageRatings, size_t n)
+{
+    if (n >= joltageRatings.size())
+    {
+        return 0;
+    }
+
+    if (adapterArrangementsMap.find(n) != adapterArrangementsMap.end())
+    {
+        return adapterArrangementsMap[n];
+    }
+
+    uint64_t possibilities = 0;
+
+    for (size_t i = 1; i < 4; i++)
+    {
+        if (n + i < joltageRatings.size() && validJoltageDiff(joltageRatings[n], joltageRatings[n + i]))
+        {
+            possibilities += findPossibilities(adapterArrangementsMap, joltageRatings, n + i);
+        }
+    }
+
+    adapterArrangementsMap[n] = possibilities;
+
+    return possibilities;
+}
+
+bool validJoltageDiff(size_t a, size_t b)
+{
+    size_t c = abs(b - a);
+    return (c >= 1 && c <=3);
+}
