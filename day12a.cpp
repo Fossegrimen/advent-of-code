fa89b6a2a27226ca101fbf487c1078bd16a88ac0
Fossegrimen <68206123+Fossegrimen@users.noreply.github.com>
2020-12-11 20:17:24 +0100

    Added day 12

---
+++ day12a.cpp
@@ -0,0 +1,74 @@
+
+#include <iostream>
+
+int main()
+{
+    char action;
+    size_t value;
+
+    ssize_t x = 0;
+    ssize_t y = 0;
+    size_t degrees = 0;
+
+    while (std::cin >> action && std::cin >> value)
+    {
+        switch (action)
+        {
+            case 'N':
+            {
+                y += value;
+                break;
+            }
+            case 'S':
+            {
+                y -= value;
+                break;
+            }
+            case 'E':
+            {
+                x += value;
+                break;
+            }
+            case 'W':
+            {
+                x -= value;
+                break;
+            }
+            case 'L':
+            {
+                degrees = (degrees + value + 360) % 360;
+                break;
+            }
+            case 'R':
+            {
+                degrees = (degrees - value + 360) % 360;
+                break;
+            }
+            case 'F':
+            {
+                if (degrees == 0)
+                {
+                    x += value;
+                }
+                else if (degrees == 90)
+                {
+                    y += value;
+                }
+                else if (degrees == 180)
+                {
+                    x -= value;
+                }
+                else if (degrees == 270)
+                {
+                    y -= value;
+                }
+
+                break;
+            }
+        }
+    }
+
+    std::cout << (abs(x) + abs(y)) << std::endl;
+
+    return 0;
+}
---
+++ day12b.cpp
@@ -0,0 +1,96 @@
+
+#include <iostream>
+
+int main()
+{
+    char action;
+    size_t value;
+
+    ssize_t w_x = 10;
+    ssize_t w_y = 1;
+    ssize_t x = 0;
+    ssize_t y = 0;
+    ssize_t degrees = 0;
+
+    while (std::cin >> action && std::cin >> value)
+    {
+        switch (action)
+        {
+            case 'N':
+            {
+                w_y += value;
+                break;
+            }
+            case 'S':
+            {
+                w_y -= value;
+                break;
+            }
+            case 'E':
+            {
+                w_x += value;
+                break;
+            }
+            case 'W':
+            {
+                w_x -= value;
+                break;
+            }
+            case 'L':
+            {
+                if (value == 90)
+                {
+                    int tmp = w_x;
+                    w_x = -w_y;
+                    w_y = tmp;
+                }
+                else if (value == 180)
+                {
+                    w_x = -w_x;
+                    w_y = -w_y;
+                }
+                else if (value == 270)
+                {
+                    int tmp = w_x;
+                    w_x = w_y;
+                    w_y = -tmp;
+                }
+
+                break;
+            }
+            case 'R':
+            {
+                if (value == 90)
+                {
+                    int tmp = w_x;
+                    w_x = w_y;
+                    w_y = -tmp;
+                }
+                else if (value == 180)
+                {
+                    w_x = -w_x;
+                    w_y = -w_y;
+                }
+                else if (value == 270)
+                {
+                    int tmp = w_x;
+                    w_x = -w_y;
+                    w_y = tmp;
+                }
+
+                break;
+            }
+            case 'F':
+            {
+                x += w_x * value;
+                y += w_y * value;
+
+                break;
+            }
+        }
+    }
+
+    std::cout << (abs(x) + abs(y)) << std::endl;
+
+    return 0;
+}
